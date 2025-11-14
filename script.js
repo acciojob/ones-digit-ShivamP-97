e03630e01c8209039d5a712ad2a4879ae0af23ef
ShivamP-97 <shivampathak19972@gmail.com>
2025-11-14 14:23:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 function onesDigit(n) {
-  // your code here
+	const num=Number(n);
+
+	if(!Number.isInteger(num)||num<=0){
+		throw new Error("Input must be a positive integer.");
+	}
+	return num%10;
 }
 
 // Do not change the code below
